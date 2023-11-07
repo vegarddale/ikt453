61775c38f1af80bffa61c40e27c25e994420747c
ErikDale <Erik.Dale@egde.no>
2023-11-07 15:26:36 +0100

    Try adding new route

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-
 from flask import Flask
+from sqlalchemy import create_engine, inspect
 
-app = Flask("test_app")
+app = Flask("Terrorist Database")
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "Hello, Flask!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+
+@app.route("/data")
+def data():
+    engine = create_engine("mysql+pymysql://user:Password123.@mysql-db/odb")
+    inspector = inspect(engine)
+    table_names = inspector.get_table_names()
+    return ", ".join(table_names)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
