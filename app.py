97605fb2d3c0570f9c0ca8dd6f4e8a88c7dea5a1
ErikDale <Erik.Dale@egde.no>
2023-11-08 10:07:19 +0100

    Move load data to separate file

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask
-from sqlalchemy import create_engine, inspect
+from load_data import data
 
 app = Flask("Terrorist Database")
-
-load_dotenv()
-
-db_username = os.getenv("DB_USERNAME")
-db_password = os.getenv("DB_PASSWORD")
-db_name = os.getenv("DB_NAME")
 
 
 @app.route("/")
@@ -18,13 +10,8 @@ def hello():
 
 
 @app.route("/data")
-def data():
-    engine = create_engine(
-        "mysql+pymysql://" + db_username + ":" + db_password + "@mysql-db/" + db_name
-    )
-    inspector = inspect(engine)
-    table_names = inspector.get_table_names()
-    return str(table_names)
+def data_route():
+    return data()
 
 
 if __name__ == "__main__":
