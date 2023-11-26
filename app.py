4fa18efa0e00755465654bfeb11d5101a2e599a9
ErikDale <Erik.Dale@egde.no>
2023-11-26 19:12:41 +0100

    Switch db from mysql to mssql

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from fetch_all_data import fetch_data
-from fetch_data_by_year import fetch_yearly_data
-from insert_data import insert_data_func
+
+# from fetch_data_by_year import fetch_yearly_data
+# from insert_data import insert_data_func
+# from fetch_region_data import fetch_region_data
 
 app = Flask("Terrorist Database")
 
@@ -16,14 +18,19 @@ def data_route():
     return fetch_data()
 
 
-@app.route("/yearly_data", methods=["GET", "POST"])
+"""@app.route("/yearly_data", methods=["GET", "POST"])
 def yearly_data_route():
     return fetch_yearly_data()
 
 
+@app.route("/region_data", methods=["GET", "POST"])
+def region_data_route():
+    return fetch_region_data()
+
+
 @app.route("/insert_data")
 def insert_route():
-    return insert_data_func()
+    return insert_data_func()"""
 
 
 if __name__ == "__main__":
