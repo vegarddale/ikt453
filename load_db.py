a2015bc5ea12866f30386f610ecc7b103d975528
vegarddale <69875804+vegarddale@users.noreply.github.com>
2023-12-14 22:29:38 +0100

    Update load_db.py

--- load_db.py
+++ load_db.py
@@ -4,24 +4,24 @@ import pymysql
 from dotenv import load_dotenv
 import os
 
+
+print("loading mysql odb...")
+
 load_dotenv()
 
-db_username = os.getenv("DB_USERNAME")
-db_password = os.getenv("DB_PASSWORD")
-db_name = os.getenv("DB_NAME")
+db_username = os.getenv("MYSQL_DB_USERNAME")
+db_password = os.getenv("MYSQL_DB_PASSWORD")
+db_name = os.getenv("MYSQL_DB_NAME")
 
-data = pd.read_excel("globalterrorismdb_0522dist.xlsx", nrows=100)
+data = pd.read_excel("globalterrorismdb_0522dist.xlsx", nrows=1000)
 
-# Create a SQLAlchemy engine with pymysql as the driver
 engine = create_engine(
     "mysql+pymysql://" + db_username + ":" + db_password + "@mysql-db/" + db_name
 )
 
-# Define the table name
 table_name = "terrorist_table"
 
-# Insert data into the table using the to_sql method
-data.to_sql(table_name, con=engine, index=False)
+data.to_sql(table_name, con=engine, index=False, if_exists='replace')
 
-# Dispose of the engine to close the database connection
 engine.dispose()
+print("mysql odb loaded")
